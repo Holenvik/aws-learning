b6b1d62a0f24e52049dc2fb8f2bb93fe1b836190
Kiryl Fiadosau <90893439+kfiadosau-clgx@users.noreply.github.com>
2023-02-16 22:58:36 +0100

    CloudFront and S3 connected, serveless configured for default region in acocunt, client:build:deploy works

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Holenvik Store
           </Link>
         </Typography>
 
