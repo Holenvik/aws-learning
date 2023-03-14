162c69edb67f1bfa43f132110b46ea697695aae1
Kiryl Fiadosau <90893439+kfiadosau-clgx@users.noreply.github.com>
2023-03-14 21:38:01 +0100

    Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://d73q1b5066.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://q11j9jr031.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
